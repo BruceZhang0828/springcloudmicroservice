bff01e7c46932dd1ba18727e2c0a4dd62f000b47
zhy <zycool0828@163.com>
2018-10-29 22:55:16 +0800

    Initial commit

---
+++ microserviceproviderdept8081/src/main/java/com/zhy/springcloud/service/DeptService.java
@@ -0,0 +1,12 @@
+package com.zhy.springcloud.service;
+
+import com.zhy.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+
+    public boolean add(Dept dept);
+    public Dept    get(Long id);
+    public List<Dept> list();
+}
