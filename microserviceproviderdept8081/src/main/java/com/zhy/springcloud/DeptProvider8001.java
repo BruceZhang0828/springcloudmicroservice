49ece1ed1d2b9800790610e805c791b86a75f617
zhy <zycool0828@163.com>
2018-11-01 20:33:35 +0800

    第二次提交

--- microserviceproviderdept8081/src/main/java/com/zhy/springcloud/DeptProvider8001.java
+++ microserviceproviderdept8081/src/main/java/com/zhy/springcloud/DeptProvider8001.java
@@ -2,8 +2,10 @@ package com.zhy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001.class,args);
